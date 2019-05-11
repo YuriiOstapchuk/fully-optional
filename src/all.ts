2cf137d642366b0cec75b4e702f1bda7bd2b3fb5
y.ost <y.ostapchuk1@gmail.com>
2019-05-11 23:03:45 +0300

    Refactor type of all function to rely on argument list tuple inference

--- src/all.ts
+++ src/all.ts
@@ -15,59 +15,14 @@ const _all = <T, R>(values: Array<T | undefined>, cb: (values: T[]) => R) => {
   return result && cb(result);
 };
 
-export function all<A1, R>(
-  values: ToOptional<[A1]>,
-  cb: (values: [A1]) => R,
+export function all<T extends any[], R>(
+  values: ToOptional<T>,
+  cb: (values: T) => R,
 ): R | undefined;
 
-export function all<A1, A2, R>(
-  values: ToOptional<[A1, A2]>,
-  cb: (values: [A1, A2]) => R,
-): R | undefined;
-
-export function all<A1, A2, A3, R>(
-  values: ToOptional<[A1, A2, A3]>,
-  cb: (values: [A1, A2, A3]) => R,
-): R | undefined;
-
-export function all<A1, A2, A3, A4, R>(
-  values: ToOptional<[A1, A2, A3, A4]>,
-  cb: (values: [A1, A2, A3, A4]) => R,
-): R | undefined;
-
-export function all<A1, A2, A3, A4, A5, R>(
-  values: ToOptional<[A1, A2, A3, A4, A5]>,
-  cb: (values: [A1, A2, A3, A4, A5]) => R,
-): R | undefined;
-
-export function all<T, R>(
-  values: Array<T | undefined>,
-  cb: (values: T[]) => R,
-): R | undefined;
-
-export function all<A1, R>(
-  cb: (values: [A1]) => R,
-): (values: ToOptional<[A1]>) => R | undefined;
-
-export function all<A1, A2, R>(
-  cb: (values: [A1, A2]) => R,
-): (values: ToOptional<[A1, A2]>) => R | undefined;
-
-export function all<A1, A2, A3, R>(
-  cb: (values: [A1, A2, A3]) => R,
-): (values: ToOptional<[A1, A2, A3]>) => R | undefined;
-
-export function all<A1, A2, A3, A4, R>(
-  cb: (values: [A1, A2, A3, A4]) => R,
-): (values: ToOptional<[A1, A2, A3, A4]>) => R | undefined;
-
-export function all<A1, A2, A3, A4, A5, R>(
-  cb: (values: [A1, A2, A3, A4, A5]) => R,
-): (values: ToOptional<[A1, A2, A3, A4, A5]>) => R | undefined;
-
-export function all<T, R>(
-  cb: (values: T[]) => R,
-): (values: Array<T | undefined>) => R | undefined;
+export function all<T extends any[], R>(
+  cb: (values: T) => R,
+): (values: ToOptional<T>) => R | undefined;
 
 export function all(...args: any) {
   return curryLast(_all, args);
